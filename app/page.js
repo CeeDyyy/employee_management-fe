11d1f8b38e208e7bbcf53746acc40bbdf9aaadf9
CeeDyyy <cdnoob@gmail.com>
2023-08-10 22:24:17 +0700

    lineliff update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,37 @@
 "use client"
+import { useState, useEffect } from 'react';
+
+const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
 export default function Home() {
+  const [profile, setProfile] = useState({})
+
+  useEffect(async () => {
+    const liff = (await import('@line/liff')).default
+    try {
+      await liff.init({ liffId });
+    } catch (error) {
+      console.error('liff init error', error.message)
+    }
+    if (liff.isLoggedIn()) {
+      const profile = await liff.getProfile()
+      setProfile(profile)
+    } else {
+      liff.login();
+    }
+  })
+
   return (
     <div>
-      asd
+      <div>
+        {profile.pictureUrl && <img
+          src={profile.pictureUrl}
+          alt={profile.displayName}
+          width={500}
+          height={500}
+        />}
+        <div>Name: {profile.displayName}</div>
+      </div>
     </div>
   )
 }
