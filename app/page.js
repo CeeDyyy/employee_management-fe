f5817490e68433421c60e10aa0b4869e5b1ed43d
Thanawat Udchachon <cdnoob@gmail.com>
2024-03-13 20:56:48 +0700

    className update

--- app/page.js
+++ app/page.js
@@ -5,18 +5,18 @@ import Link from "next/link"
 export default function Home() {
 
   return (
-    <div className="h-screen mx-4 md:mx-96 grid md:grid-cols-2 gap-4 md:gap-16 content-center place-content-stretch md:place-content-center">
-      <Link href="/document" className="flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 rounded-lg shadow-md">
-        <p className="text-white">เอกสาร</p>
+    <div className="h-screen mx-4 md:mx-48 2xl:mx-72 grid md:grid-cols-2 gap-4 md:gap-16 content-center place-content-stretch md:place-content-center">
+      <Link href="/document" className="group/document flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
+        <p className="group-hover/document:scale-110 duration-200 text-white">เอกสาร</p>
       </Link>
-      <Link href="/calendar" className="flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 rounded-lg shadow-md">
-        <p className="text-white">ปฏิทิน</p>
+      <Link href="/calendar" className="group/calendar flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
+        <p className="group-hover/calendar:scale-110 duration-200 text-white">ปฏิทิน</p>
       </Link>
-      <Link href="/calendar?detailpanel=leave&display=horizontal&ifdata=true&onlyme=true" className="flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 rounded-lg shadow-md">
-        <p className="text-white">ลา</p>
+      <Link href="/calendar?detailpanel=leave&display=horizontal&ifdata=true&onlyme=true" className="group/leave flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
+        <p className="group-hover/leave:scale-110 duration-200 text-white">ลา</p>
       </Link>
-      <Link href="/calendar?detailpanel=booking&display=horizontal&ifdata=true&onlyme=true" className="flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 rounded-lg shadow-md">
-        <p className="text-white">จองรถ</p>
+      <Link href="/calendar?detailpanel=booking&display=horizontal&ifdata=true&onlyme=true" className="group/booking flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
+        <p className="group-hover/booking:scale-110 duration-200 text-white">จองรถ</p>
       </Link>
     </div>
   )
--- components/layouts.js
+++ components/layouts.js
@@ -5,9 +5,9 @@ import BackBtn from './backbtn';
 const Page = ({ children, title = "title", path = "/" }) => {
     return (
         <div className="h-screen mx-4 md:mx-48 2xl:mx-72">
-            <div className="pt-2 fixed w-full grid grid-cols-3 content-center bg-neutral-50">
+            <div className="sticky pt-2 w-full grid grid-cols-3 content-center bg-neutral-50">
                 <BackBtn path={path} />
-                <p className="-ml-9 text-center text-2xl font-bold self-center">{title}</p>
+                <p className="text-center text-2xl font-bold self-center">{title}</p>
             </div>
             <div className="mt-14 mb-11">
             {children}
