6c0b3e1898afdc98426c37d1185ca730be9f40be
Thanawat Udchachon <cdnoob@gmail.com>
2024-03-14 11:12:43 +0700

    use .map instead

--- app/page.js
+++ app/page.js
@@ -3,21 +3,32 @@
 import Link from "next/link"
 
 export default function Home() {
+  const pages = [
+    {
+      title: "เอกสาร",
+      href: "/document"
+    },
+    {
+      title: "ปฏิทิน",
+      href: "/calendar"
+    },
+    {
+      title: "ลา",
+      href: "/calendar?detailpanel=leave&display=horizontal&ifdata=true&onlyme=true"
+    },
+    {
+      title: "จองรถ",
+      href: "/calendar?detailpanel=booking&display=horizontal&ifdata=true&onlyme=true"
+    }
+  ]
 
   return (
     <div className="h-screen mx-4 md:mx-48 2xl:mx-72 grid md:grid-cols-2 gap-4 md:gap-16 content-center place-content-stretch md:place-content-center">
-      <Link href="/document" className="group/document flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
-        <p className="group-hover/document:scale-110 duration-200 text-white">เอกสาร</p>
-      </Link>
-      <Link href="/calendar" className="group/calendar flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
-        <p className="group-hover/calendar:scale-110 duration-200 text-white">ปฏิทิน</p>
-      </Link>
-      <Link href="/calendar?detailpanel=leave&display=horizontal&ifdata=true&onlyme=true" className="group/leave flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
-        <p className="group-hover/leave:scale-110 duration-200 text-white">ลา</p>
-      </Link>
-      <Link href="/calendar?detailpanel=booking&display=horizontal&ifdata=true&onlyme=true" className="group/booking flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md">
-        <p className="group-hover/booking:scale-110 duration-200 text-white">จองรถ</p>
-      </Link>
+      {pages.map((page, index) =>
+        <Link href={page.href} className="group flex justify-center items-center h-20 md:h-48 cursor-pointer bg-cyan-500 hover:saturate-150 active:scale-105 duration-150 rounded-lg shadow-md" key={index}>
+          <p className="group-hover:scale-110 duration-200 text-white">{page.title}</p>
+        </Link>
+      )}
     </div>
   )
 }
