e196b4fb8230493a036a205fae8f9672b8572068
CeeDyyy <cdnoob@gmail.com>
2026-04-15 02:30:59 +0700

    added more detail

--- app/calendar/components/BookingDetail.js
+++ app/calendar/components/BookingDetail.js
@@ -102,26 +102,27 @@ const BookingDetail = ({ booking }) => {
                 <p>ผู้ให้อนุญาต: </p><p className="font-bold">{booking.approver}</p><p>เวลา: </p><p className="font-bold">{booking.update_timestamp}</p>
                 <p>เหตุผล: </p><p className="col-span-3 font-bold">{booking.reason}</p>
             </div> */}
-            <div className="p-4 grid grid-cols-12 gap-y-2 items-center bg-sky-50 rounded-b-lg border-2 border-white">
-                <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#06b6d4" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
-                <p className="col-span-5 font-bold">ประเภท</p>
+            <div className="p-4 grid grid-cols-12 gap-y-2 items-start bg-sky-50 rounded-b-lg border-2 border-white">
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#06b6d4" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
+                <p className="col-span-5 font-bold">ประเภทการลา</p>
                 <p className="col-span-6">{booking.type}</p>
-                <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M12 7V12L14.5 13.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#06b6d4" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
-                <p className="col-span-5 font-bold">ระยะเวลา</p>
-                <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M3 5.5L5 3.5M21 5.5L19 3.5M12 8.5V12.5L14 14.5M20 12.5C20 16.9183 16.4183 20.5 12 20.5C7.58172 20.5 4 16.9183 4 12.5C4 8.08172 7.58172 4.5 12 4.5C16.4183 4.5 20 8.08172 20 12.5Z" stroke="#06b6d4" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
-                <p className="col-span-5">{booking.start_date.slice(-5)} - {booking.end_date.slice(-5)} น.</p>
+                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#06b6d4" stroke-width="2" stroke-linecap="round"></path> </g></svg>
+                <p className="col-span-5 font-bold">ระยะเวลา<br/><span className="font-extralight">(เริ่มต้น)</span></p>
+                <p className="col-span-6">{convertDateToThai(format(new Date(booking.start_date), 'd MMMM yyyy'))} {booking.start_date.slice(-5)}น.</p>
+                <p className="col-start-2 col-span-5 font-extralight">(สิ้นสุด)</p>
+                <p className="col-span-6">{convertDateToThai(format(new Date(booking.end_date), 'd MMMM yyyy'))} {booking.end_date.slice(-5)}น.</p>
                 <p className="col-start-2 col-span-5 font-bold">เหตุผล</p>
                 <p className="col-span-6">{booking.detail}</p>
                 <p className="col-start-2 col-span-5 font-bold">แจ้งโดย</p>
                 <p className="col-span-6">{booking.user.fullname}</p>
                 <p className="col-start-2 col-span-5 font-bold">ส่งคำขอเมื่อ</p>
-                <p className="col-span-6">{convertDateToThai(format(new Date(booking.timestamp), 'yyyy MMM dd HH:mm'))} น.</p>
+                <p className="col-span-6">{convertDateToThai(format(new Date(booking.timestamp), 'd MMMM yyyy HH:mm'))} น.</p>
                 <p className="col-start-2 col-span-5 font-bold">ผู้ให้อนุญาต</p>
                 <p className="col-span-6">{booking.approver || "-"}</p>
                 <p className="col-start-2 col-span-5 font-bold">ผลการพิจารณา</p>
                 <p className="col-span-6">{statusTH[booking.status]}</p>
                 <p className="col-start-2 col-span-5 font-bold">ลงบันทึกเมื่อ</p>
-                <p className="col-span-6">{convertDateToThai(format(new Date(booking.updated), 'yyyy MMM dd HH:mm'))} น.</p>
+                <p className="col-span-6">{convertDateToThai(format(new Date(booking.updated), 'd MMMM yyyy HH:mm'))} น.</p>
                 <button className="col-span-6 flex justify-center items-center gap-1 m-1 p-2 text-blue-500 bg-[radial-gradient(50%_70%_at_center,_var(--tw-gradient-stops))] from-blue-100 to-white border-2 border-white rounded-xl shadow-md">
                     <svg className="-mb-0.5" width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M15.8787 3.70705C17.0503 2.53547 18.9498 2.53548 20.1213 3.70705L20.2929 3.87862C21.4645 5.05019 21.4645 6.94969 20.2929 8.12126L18.5556 9.85857L8.70713 19.7071C8.57897 19.8352 8.41839 19.9261 8.24256 19.9701L4.24256 20.9701C3.90178 21.0553 3.54129 20.9554 3.29291 20.7071C3.04453 20.4587 2.94468 20.0982 3.02988 19.7574L4.02988 15.7574C4.07384 15.5816 4.16476 15.421 4.29291 15.2928L14.1989 5.38685L15.8787 3.70705ZM18.7071 5.12126C18.3166 4.73074 17.6834 4.73074 17.2929 5.12126L16.3068 6.10738L17.8622 7.72357L18.8787 6.70705C19.2692 6.31653 19.2692 5.68336 18.8787 5.29283L18.7071 5.12126ZM16.4477 9.13804L14.8923 7.52185L5.90299 16.5112L5.37439 18.6256L7.48877 18.097L16.4477 9.13804Z" fill="#3b82f6" /> </g></svg>
                     แก้ไขคำขอ
